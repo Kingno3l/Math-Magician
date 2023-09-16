0912a6bdbd5ac4987bf7e4f321cfee19ca593307
kingno3l <nuelluiz@gmail.com>
2023-09-16 13:21:20 +0100

    linters check

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -1,24 +1,23 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-axios.defaults.headers.common["X-Api-Key"] =
-  "X0RJf1RjhctUxTka7UQisg==TgVq8OcBO5Lh3LUw";
+axios.defaults.headers.common['X-Api-Key'] = 'X0RJf1RjhctUxTka7UQisg==TgVq8OcBO5Lh3LUw';
 
 export default function Quotes() {
-  const [quote, setQuote] = useState("Loading...");
-  const [error, setError] = useState("");
+  const [quote, setQuote] = useState('Loading...');
+  const [error, setError] = useState('');
 
   useEffect(() => {
     const fetchQuote = async () => {
       try {
         const response = await axios.get(
-          "https://api.api-ninjas.com/v1/quotes?category=success"
+          'https://api.api-ninjas.com/v1/quotes?category=success',
         );
         if (response.status === 200) {
           setQuote(response.data[0].quote);
         }
       } catch (e) {
-        setError("An error occurred while fetching the quote.");
+        setError('An error occurred while fetching the quote.');
       }
     };
 
